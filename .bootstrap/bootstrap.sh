109d47abf57b9d068e2db4e80247a7d5a4b9838c
molasun <mola79321@gmail.com>
2024-10-03 14:32:56 +0800

    update bootstrap.sh

--- .bootstrap/bootstrap.sh
+++ .bootstrap/bootstrap.sh
@@ -1,23 +1,25 @@
 # /bin/bash
 
-if [ "$#" -ne 2 ]; then
-    echo "usage: ./bootstrap.sh [your gitops repo] [your cluster name]"
+if [ "$#" -ne 3 ]; then
+    echo "usage: ./bootstrap.sh [your gitops repo] [your cluster name] [admin username]"
     exit 1
 fi
 
 unset $1
 unset $2
+unset $3
 
 export gitops_repo=$(echo "${1:-https://github.com/CCChou/OpenShift-EaaS-Practice.git}") #<your newly created repo>
 export cluster_name=$(echo "${2:-hub}")	 #<your cluster name, default hub>
 export cluster_base_domain=$(oc get ingress.config.openshift.io cluster --template={{.spec.domain}} | sed -e "s/^apps.//")
 export platform_base_domain=${cluster_base_domain#*.}
+export admin_username==$(echo "${3:-admin}")  #<your admin username, default admin>
 
-oc apply -f .bootstrap/group.yaml
+envsubst < .bootstrap/group.yaml | oc apply -f -
 
 oc apply -f .bootstrap/subscription.yaml
 echo -n "Waiting for openshift-gitops operators ready in openshift-operators namespace"
-while [ "READY" != "$(oc get subscription openshift-gitops-operator -n openshift-operators -ojsonpath='{.items[0].status.conditions[0].type}')" ]; do
+while [ "true" != "$(oc get pod -l control-plane=gitops-operator -n openshift-operators -ojsonpath='{.items[0].status.containerStatuses[0].ready}')" ]; do
     echo -n '.'
     sleep 3
 done
@@ -27,7 +29,7 @@ oc apply -f .bootstrap/cluster-rolebinding.yaml
 
 envsubst < .bootstrap/argocd.yaml | oc apply -f -
 echo -n "Waiting for argocd server ready in openshift-gitops namespace"
-while [ "READY" != "$(oc get argocd openshift-gitops -n openshift-gitops -ojsonpath='{.items[0].status.conditions[0].type}')" ]; do
+while [ "true" != "$(oc get pod -l app.kubernetes.io/name=openshift-gitops-server -n openshift-gitops -ojsonpath='{..items[0].status.containerStatuses[0].ready}')" ]; do
     echo -n '.'
     sleep 3
 done
