4849949b2e2ff462c8fa6f2b5781222cfeedf039
dennischou <dses5900@gmail.com>
2024-10-03 21:41:25 +0800

    Add new parameter pin

--- .bootstrap/bootstrap.sh
+++ .bootstrap/bootstrap.sh
@@ -1,19 +1,21 @@
 # /bin/bash
 
-if [ "$#" -ne 3 ]; then
-    echo "usage: ./bootstrap.sh [your gitops repo] [your cluster name] [admin username]"
+if [ "$#" -gt 5 ]; then
+    echo "usage: ./bootstrap.sh [your gitops repo] [your cluster name] [admin username] [pin]"
     exit 1
 fi
 
 unset $1
 unset $2
 unset $3
+unset $4
 
 export gitops_repo=$(echo "${1:-https://github.com/CCChou/OpenShift-EaaS-Practice.git}") #<your newly created repo>
 export cluster_name=$(echo "${2:-hub}")	 #<your cluster name, default hub>
 export cluster_base_domain=$(oc get ingress.config.openshift.io cluster --template={{.spec.domain}} | sed -e "s/^apps.//")
 export platform_base_domain=${cluster_base_domain#*.}
-export admin_username==$(echo "${3:-admin}")  #<your admin username, default admin>
+export admin_username=$(echo "${3:-admin}")  #<your admin username, default admin>
+export pin=$(echo "${4:-HEAD}")
 
 envsubst < .bootstrap/group.yaml | oc apply -f -
 
