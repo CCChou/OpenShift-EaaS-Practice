fcccfe63805c9da441d928c3702ff5ce5a92f670
molasun <mola79321@gmail.com>
2024-10-04 16:06:43 +0800

    update bootstrap.sh

--- .bootstrap/bootstrap.sh
+++ .bootstrap/bootstrap.sh
@@ -11,11 +11,11 @@ unset $3
 unset $4
 
 export gitops_repo=$1 #<your newly created repo>
-export cluster_name=$2	 #<your cluster name, default hub>
+export cluster_name=$2 #<your cluster name, default hub>
 export cluster_base_domain=$(oc get ingress.config.openshift.io cluster --template={{.spec.domain}} | sed -e "s/^apps.//")
 export platform_base_domain=${cluster_base_domain#*.}
 export admin_username=$3  #<your admin username, default admin>
-export pin=$4
+export pin=$4 #<your target revision>
 
 envsubst < .bootstrap/group.yaml | oc apply -f -
 
